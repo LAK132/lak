19991665e1b1357be1bb7ae8515ad73f74acc752
LAK132 <lak132@github.com>
2020-05-10 13:35:57 +0930

    Add OS checking

---
+++ inc/lak/os.hpp
@@ -0,0 +1,29 @@
+#ifndef LAK_OS_HPP
+#define LAK_OS_HPP
+
+// https://sourceforge.net/p/predef/wiki/OperatingSystems/
+// https://web.archive.org/web/20200422000946/https://sourceforge.net/p/predef/wiki/OperatingSystems/
+
+#if defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) ||               \
+  defined(__WIN64__) || defined(__TOS_WIN__) || defined(__WINDOWS__)
+#  define LAK_OS_WINDOWS
+#endif
+
+#if defined(__APPLE__) || defined(__MACH__)
+#  define LAK_OS_APPLE
+#endif
+
+#if defined(__ANDROID__)
+#  define LAK_OS_ANDROID
+#endif
+
+#if defined(__gnu_linux__)
+#  define LAK_OS_GNU_LINUX
+#endif
+
+#if defined(__linux__)
+// *Anything* running the Linux kernel.
+#  define LAK_OS_LINUX
+#endif
+
+#endif
