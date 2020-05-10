66421358db0c22f2ea0feba86a591c77849c082e
LAK132 <lak132@github.com>
2020-05-10 13:35:44 +0930

    Add more file functions

--- inc/lak/file.hpp
+++ inc/lak/file.hpp
@@ -4,6 +4,7 @@
 #include "string.hpp"
 
 #include <filesystem>
+#include <tuple>
 #include <vector>
 
 namespace lak
@@ -19,6 +20,25 @@ namespace lak
   bool save_file(const fs::path &path, const std::u8string &string);
   bool save_file(const fs::path &path, const std::u16string &string);
   bool save_file(const fs::path &path, const std::u32string &string);
+
+  fs::path exe_path();
+
+  fs::path normalised(const fs::path &path);
+
+  bool has_parent(const fs::path &path);
+
+  // The the ACTUAL parent path.
+  // fs::path::parent_path() incorrectly gets "a/b" from "a/b/", this function
+  // would instead return "a/" by normalising to "a/b/." first.
+  fs::path parent(const fs::path &path);
+
+  struct deepest_folder_result
+  {
+    fs::path folder;
+    fs::path file;
+  };
+  deepest_folder_result deepest_folder(const fs::path &path,
+                                       std::error_code &ec);
 }
 
 #endif
