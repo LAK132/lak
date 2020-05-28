d510fb8d92b317f7ef4d57c38d894bb892f35593
LAK132 <lak132@github.com>
2020-05-28 16:37:28 +0930

    Add stdint.hpp

---
+++ inc/lak/stdint.hpp
@@ -0,0 +1,12 @@
+#ifndef LAK_STDINT_HPP
+#define LAK_STDINT_HPP
+
+#include <cstddef>
+#include <cstdint>
+
+namespace lak
+{
+  static const constexpr size_t dynamic_extent = SIZE_MAX;
+}
+
+#endif
