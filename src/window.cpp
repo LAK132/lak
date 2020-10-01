e12ca1bb8d3ef6be643e4b4eabac8d671d02069e
LAK132 <LAK132@kicodora.com>
2020-10-01 23:26:03 +0930

    Better namespacing

--- src/window.cpp
+++ src/window.cpp
@@ -2,21 +2,18 @@
 
 #include <thread>
 
-namespace lak
+uint64_t lak::yield_frame(const uint64_t last_counter,
+                          const uint32_t target_framerate)
 {
-  uint64_t yield_frame(const uint64_t last_counter,
-                       const uint32_t target_framerate)
-  {
-    const uint64_t target_count =
-      (performance_frequency() / target_framerate) + last_counter;
+  const uint64_t target_count =
+    (lak::performance_frequency() / target_framerate) + last_counter;
 
-    for (uint64_t count = performance_counter();
-         target_count > last_counter
-           ? (count < target_count)
-           : (count < target_count || count > last_counter);
-         count = performance_counter())
-      std::this_thread::yield();
+  for (uint64_t count = lak::performance_counter();
+       target_count > last_counter
+         ? (count < target_count)
+         : (count < target_count || count > last_counter);
+       count = lak::performance_counter())
+    std::this_thread::yield();
 
-    return performance_counter();
-  }
+  return lak::performance_counter();
 }
