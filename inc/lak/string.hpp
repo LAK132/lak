b90ba1831f4dde1208efa3780adf3544308cf5a6
LAK132 <LAK132@kicodora.com>
2020-10-01 22:41:28 +0930

    Add lak::is_string<T>

--- inc/lak/string.hpp
+++ inc/lak/string.hpp
@@ -14,6 +14,19 @@ namespace lak
   using u8string  = lak::string<char8_t>;
   using u16string = lak::string<char16_t>;
   using u32string = lak::string<char32_t>;
+
+  template<typename T>
+  struct is_string
+  {
+    static constexpr bool value = false;
+  };
+  template<typename CHAR>
+  struct is_string<lak::string<CHAR>>
+  {
+    static constexpr bool value = true;
+  };
+  template<typename T>
+  inline constexpr bool is_string_v = lak::is_string<T>::value;
 }
 
 #endif
