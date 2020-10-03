faca9822a9cf0eea88d3ad3033e17ae2d2fdac1b
LAK132 <LAK132@kicodora.com>
2020-10-03 17:12:50 +0930

    Add packed_struct

--- inc/lak/compiler.hpp
+++ inc/lak/compiler.hpp
@@ -2,17 +2,19 @@
 #define LAK_COMPILER_HPP
 
 #if defined(__clang__)
-#  define force_inline inline __attribute__((always_inline))
+#  define force_inline  inline __attribute__((always_inline))
+#  define packed_struct struct __attribute__((packed))
 #  define DEBUG_BREAK()
 #elif defined(__GNUC__)
-#  define force_inline inline __attribute__((always_inline))
+#  define force_inline  inline __attribute__((always_inline))
+#  define packed_struct struct __attribute__((packed))
 #  define DEBUG_BREAK()
 #elif defined(_MSC_VER)
 #  define force_inline  inline __forceinline
+#  define packed_struct __pragma(pack(1)) struct
 #  define DEBUG_BREAK() __debugbreak()
 #else
-#  define force_inline inline
-#  define DEBUG_BREAK()
+#  error "Compiler not supported"
 #endif
 
 #endif
