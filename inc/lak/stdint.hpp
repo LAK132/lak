3c4910eac679ce9a1eb165abffaf74b2b99d9f82
LAK132 <lakridesagain@gmail.com>
2020-10-31 03:29:42 +1030

    Add f32_t and f64_t

--- inc/lak/stdint.hpp
+++ inc/lak/stdint.hpp
@@ -9,4 +9,10 @@ namespace lak
   static const constexpr size_t dynamic_extent = SIZE_MAX;
 }
 
+using f32_t = float;
+using f64_t = double;
+
+static_assert(sizeof(f32_t) == 4);
+static_assert(sizeof(f64_t) == 8);
+
 #endif
