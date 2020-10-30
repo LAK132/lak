804c600af1d3006c92681f1d133f191fc11da1f4
LAK132 <lakridesagain@gmail.com>
2020-10-30 22:22:30 +1030

    Add LAK_COMPILER_{CLANG,GNUC,MSVC}

--- inc/lak/compiler.hpp
+++ inc/lak/compiler.hpp
@@ -1,15 +1,27 @@
 #ifndef LAK_COMPILER_HPP
 #define LAK_COMPILER_HPP
 
-#if defined(__clang__)
+#ifdef __clang__
+#  define LAK_COMPILER_CLANG
+#endif
+
+#ifdef __GNUC__
+#  define LAK_COMPILER_GNUC
+#endif
+
+#ifdef _MSC_VER
+#  define LAK_COMPILER_MSVC
+#endif
+
+#if defined(LAK_COMPILER_CLANG)
 #  define force_inline  inline __attribute__((always_inline))
 #  define packed_struct struct __attribute__((packed))
 #  define DEBUG_BREAK()
-#elif defined(__GNUC__)
+#elif defined(LAK_COMPILER_GNUC)
 #  define force_inline  inline __attribute__((always_inline))
 #  define packed_struct struct __attribute__((packed))
 #  define DEBUG_BREAK()
-#elif defined(_MSC_VER)
+#elif defined(LAK_COMPILER_MSVC)
 #  define force_inline  inline __forceinline
 #  define packed_struct __pragma(pack(1)) struct
 #  define DEBUG_BREAK() __debugbreak()
