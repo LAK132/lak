1173efb2d6b7284ae2091e0d769081112a97c8f1
LAK132 <lakridesagain@gmail.com>
2020-10-31 03:32:10 +1030

    Add wide type typedefs (and unions for gcc)

--- inc/lak/intrin.hpp
+++ inc/lak/intrin.hpp
@@ -34,6 +34,95 @@ namespace lak
 
     static instruction_set get();
   };
+
+#if defined(LAK_COMPILER_MSVC)
+  using m128_t  = __m128;
+  using m256_t  = __m256;
+  using m256d_t = __m256d;
+  using m128i_t = __m128i;
+  using m256i_t = __m256i;
+#elif defined(LAK_COMPILER_GNUC)
+  union alignas(__m128) m128_t
+  {
+    __m128 m128;
+
+    f32_t m128_f32[4];
+
+    m128_t() = default;
+    m128_t(const __m128 &v) : m128(v) {}
+    operator __m128() const { return m128; }
+    operator __m128 &() { return m128; }
+    operator const __m128 &() const { return m128; }
+  };
+
+  union alignas(__m256) m256_t
+  {
+    __m256 m256;
+
+    f32_t m256_f32[8];
+
+    m256_t() = default;
+    m256_t(const __m256 &v) : m256(v) {}
+    operator __m256() const { return m256; }
+    operator __m256 &() { return m256; }
+    operator const __m256 &() const { return m256; }
+  };
+
+  union alignas(__m256d) m256d_t
+  {
+    __m256d m256d;
+
+    f64_t m256d_f64[4];
+
+    m256d_t() = default;
+    m256d_t(const __m256d &v) : m256d(v) {}
+    operator __m256d() const { return m256d; }
+    operator __m256d &() { return m256d; }
+    operator const __m256d &() const { return m256d; }
+  };
+
+  union alignas(__m128i) m128i_t
+  {
+    __m128i m128i;
+
+    int8_t m128i_i8[16];
+    int16_t m128i_i16[8];
+    int32_t m128i_i32[4];
+    int64_t m128i_i64[2];
+    uint8_t m128i_u8[16];
+    uint16_t m128i_u16[8];
+    uint32_t m128i_u32[4];
+    uint64_t m128i_u64[2];
+
+    m128i_t() = default;
+    m128i_t(const __m128i &v) : m128i(v) {}
+    operator __m128i() const { return m128i; }
+    operator __m128i &() { return m128i; }
+    operator const __m128i &() const { return m128i; }
+  };
+
+  union alignas(__m256i) m256i_t
+  {
+    __m256i m256i;
+
+    int8_t m256i_i8[32];
+    int16_t m256i_i16[16];
+    int32_t m256i_i32[8];
+    int64_t m256i_i64[4];
+    uint8_t m256i_u8[32];
+    uint16_t m256i_u16[16];
+    uint32_t m256i_u32[8];
+    uint64_t m256i_u64[4];
+
+    m256i_t() = default;
+    m256i_t(const __m256i &v) : m256i(v) {}
+    operator __m256i() const { return m256i; }
+    operator __m256i &() { return m256i; }
+    operator const __m256i &() const { return m256i; }
+  };
+#else
+#  error "Compiler not supported"
+#endif
 }
 
 #endif
