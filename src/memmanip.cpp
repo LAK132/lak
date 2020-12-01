13e444a3cd08d4665f908ee84b1d8b4319ab5e40
LAK132 <lakridesagain@gmail.com>
2020-12-01 20:18:36 +1030

    More page manipulation stuff

--- src/memmanip.cpp
+++ src/memmanip.cpp
@@ -17,28 +17,39 @@
 #  include <unistd.h>
 #endif
 
-lak::span<void> lak::page_reserve(size_t size)
+size_t lak::page_size()
 {
-  ASSERT_GREATER(size, 0);
-  void *result = nullptr;
 #if defined(LAK_OS_WINDOWS)
   const static DWORD page_size = []() {
     SYSTEM_INFO system_info;
     GetSystemInfo(&system_info);
     return system_info.dwPageSize;
   }();
-  // round size to the nearest page size
-  size   = page_size * ((size % page_size > 1 ? 1 : 0) + (size / page_size));
-  result = VirtualAlloc(nullptr, size, MEM_RESERVE, PAGE_READWRITE);
 #elif defined(LAK_OS_LINUX)
   const static long page_size = sysconf(_SC_PAGESIZE);
-  // round size to the nearest page size
-  size   = page_size * ((size % page_size > 1 ? 1 : 0) + (size / page_size));
-  int fd = open("/dev/zero", O_RDWR);
-  result = mmap(nullptr, size, PROT_NONE, MAP_PRIVATE | MAP_FILE, fd, 0);
 #else
 #  error "OS not supported"
 #endif
+  return page_size;
+}
+
+lak::span<void> lak::page_reserve(size_t size)
+{
+  ASSERT_GREATER(size, 0);
+  void *result           = nullptr;
+  const size_t page_size = lak::page_size();
+  // round size to the nearest page size
+  size = page_size * ((size % page_size > 1 ? 1 : 0) + (size / page_size));
+
+#if defined(LAK_OS_WINDOWS)
+  result = VirtualAlloc(nullptr, size, MEM_RESERVE, PAGE_READWRITE);
+#elif defined(LAK_OS_LINUX)
+  if (int fd = open("/dev/zero", O_RDWR); fd >= 0)
+    result = mmap(nullptr, size, PROT_NONE, MAP_PRIVATE | MAP_FILE, fd, 0);
+#else
+#  error "OS not supported"
+#endif
+
   return lak::span<void>(result, result ? size : 0);
 }
 
@@ -49,6 +60,22 @@ bool lak::page_commit(lak::span<void> pages)
            pages.data(), pages.size(), MEM_COMMIT, PAGE_READWRITE) != nullptr;
 #elif defined(LAK_OS_LINUX)
   return mprotect(pages.data(), pages.size(), PROT_READ | PROT_WRITE) == 0;
+#else
+#  error "OS not supported"
+#endif
+}
+
+bool lak::page_decommit(lak::span<void> pages)
+{
+// https://bugzilla.mozilla.org/show_bug.cgi?id=670596
+#if defined(LAK_OS_WINDOWS)
+  return VirtualFree(pages.data(), pages.size(), MEM_DECOMMIT) != 0;
+#elif defined(LAK_OS_LINUX)
+  return mprotect(pages.data(), pages.size(), PROT_NONE) == 0 &&
+         madvice(pages.data(), pages.size(), MADV_DONTNEED) == 0;
+#elif defined(LAK_OS_MAC)
+  return mprotect(pages.data(), pages.size(), PROT_NONE) == 0 &&
+         madvice(pages.data(), pages.size(), MADV_FREE) == 0;
 #else
 #  error "OS not supported"
 #endif
