2c63beb37e1f490f03a2821890418bf505392533
LAK132 <lakridesagain@gmail.com>
2021-03-12 00:46:10 +1030

    Add detected type traits

--- inc/lak/type_traits.hpp
+++ inc/lak/type_traits.hpp
@@ -6,10 +6,81 @@
 
 namespace lak
 {
-  /* --- is_constructible --- */
-
   template<typename...>
   using void_t = void;
+
+  struct nonesuch
+  {
+    ~nonesuch()                = delete;
+    nonesuch(const nonesuch &) = delete;
+    void operator=(const nonesuch &) = delete;
+  };
+
+  /* --- detected_or --- */
+
+  template<typename DEFAULT,
+           typename V,
+           template<typename...>
+           typename OP,
+           typename... ARGS>
+  struct _detected
+  {
+    using value_t = lak::false_type;
+    using type    = DEFAULT;
+  };
+
+  template<typename DEFAULT,
+           template<typename...>
+           typename OP,
+           typename... ARGS>
+  struct _detected<DEFAULT, void_t<OP<ARGS...>>, OP, ARGS...>
+  {
+    using value_t = lak::true_type;
+    using type    = OP<ARGS...>;
+  };
+
+  template<typename DEFAULT,
+           template<typename...>
+           typename OP,
+           typename... ARGS>
+  using detected_or = lak::_detected<DEFAULT, void, OP, ARGS...>;
+
+  template<typename DEFAULT,
+           template<typename...>
+           typename OP,
+           typename... ARGS>
+  using detected_or_t = typename lak::detected_or<DEFAULT, OP, ARGS...>::type;
+
+  template<template<typename...> typename OP, typename... ARGS>
+  using detected_t =
+    typename lak::detected_or<lak::nonesuch, OP, ARGS...>::type;
+
+  /* --- is_detected --- */
+
+  template<template<typename...> typename OP, typename... ARGS>
+  using is_detected =
+    typename lak::detected_or<lak::nonesuch, OP, ARGS...>::value_t;
+
+  template<template<typename...> typename OP, typename... ARGS>
+  constexpr inline bool is_detected_v = lak::is_detected<OP, ARGS...>::value;
+
+  /* --- is_detected_exact --- */
+
+  template<typename EXPECTED,
+           template<typename...>
+           typename OP,
+           typename... ARGS>
+  using is_detected_exact =
+    lak::is_same<EXPECTED, lak::detected_t<OP, ARGS...>>;
+
+  template<typename EXPECTED,
+           template<typename...>
+           typename OP,
+           typename... ARGS>
+  constexpr inline bool is_detected_exact_v =
+    lak::is_deteccted_exact<EXPECTED, OP, ARGS...>::value;
+
+  /* --- is_constructible --- */
 
   template<typename, typename T, typename... ARGS>
   struct is_constructible_ : lak::false_type
