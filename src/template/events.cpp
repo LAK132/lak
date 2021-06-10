f35ba918758939122f9403111296f6d0bd433488
LAK132 <lakridesagain@gmail.com>
2021-06-10 22:13:19 +0930

    add backend template

---
+++ src/template/events.cpp
@@ -0,0 +1,15 @@
+#include "lak/events.hpp"
+
+bool lak::next_event(const lak::platform_instance &i, lak::event *e)
+{
+  (void)i;
+  (void)e;
+  return false;
+}
+
+bool lak::peek_event(const lak::platform_instance &i, lak::event *e)
+{
+  (void)i;
+  (void)e;
+  return false;
+}
