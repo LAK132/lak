0122d6edaf8cd431ed717a5b09ede01aba95e32b
LAK132 <lakridesagain@gmail.com>
2021-06-14 14:11:09 +0930

    simplify templated memcpy and memmove

--- inc/lak/memmanip.hpp
+++ inc/lak/memmanip.hpp
@@ -24,18 +24,16 @@ namespace lak
 
   void memmove(lak::span<char> dst, lak::span<const char> src);
 
-  template<typename T, typename U>
-  force_inline void memcpy(T *dst, const U *src)
+  template<typename T>
+  force_inline void memcpy(T *dst, const T *src)
   {
-    static_assert(std::is_same_v<T, U>, "Cannot memcpy different types");
-    std::memcpy(dst, src, sizeof(T));
+    lak::memcpy(lak::as_bytes(dst), lak::as_bytes(src));
   }
 
-  template<typename T, typename U>
-  force_inline void memmove(T *dst, const U *src)
+  template<typename T>
+  force_inline void memmove(T *dst, const T *src)
   {
-    static_assert(std::is_same_v<T, U>, "Cannot memmove different types");
-    std::memmove(dst, src, sizeof(T));
+    lak::memmove(lak::as_bytes(dst), lak::as_bytes(src));
   }
 
   template<typename T>
