49ef3f69ef644312d89ee374504a6fe21a6ddc99
LAK132 <lakridesagain@gmail.com>
2021-06-21 23:18:09 +0930

    add lak::error_code_error/lak::error_code_result

---
+++ inc/lak/error_code_result.hpp
@@ -0,0 +1,31 @@
+#ifndef LAK_ERROR_CODE_RESULT_HPP
+#define LAK_ERROR_CODE_RESULT_HPP
+
+#include "lak/result.hpp"
+#include "lak/strconv.hpp"
+#include "lak/streamify.hpp"
+
+#include <cstring>
+#include <ostream>
+#include <system_error>
+
+namespace lak
+{
+  struct error_code_error
+  {
+    std::error_code value;
+
+    inline lak::astring to_string() const { return value.message(); }
+  };
+
+  template<typename T = lak::monostate>
+  using error_code_result = lak::result<T, error_code_error>;
+}
+
+[[maybe_unused]] static std::ostream &operator<<(
+  std::ostream &strm, const lak::error_code_error &err)
+{
+  return strm << err.to_string();
+}
+
+#endif
