1e69228f1b9e5cdf3dbafd12f17cdf0707823d9f
LAK132 <lakridesagain@gmail.com>
2021-06-24 20:54:55 +0930

    don't support arbitrary clearing, require complete reset via re-construction

--- inc/lak/bit_reader.hpp
+++ inc/lak/bit_reader.hpp
@@ -40,21 +40,6 @@ namespace lak
 
     inline void reset_data(lak::span<const uint8_t> data) { _data = data; }
 
-    inline void clear_data() { _data = {}; }
-
-    inline void clear_bits()
-    {
-      _bit_accum   = 0;
-      _num_bits    = 0;
-      _unused_bits = 8;
-    }
-
-    inline void clear()
-    {
-      clear_data();
-      clear_bits();
-    }
-
     enum struct error_t : uint8_t
     {
       // insufficent data left. this signifies that you should reset_data.
