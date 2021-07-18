2ecd8eab0f34ae3da11402490e8de7ffdcc7bcf7
LAK132 <lakridesagain@gmail.com>
2021-07-19 02:45:27 +0930

    add lak::close_to

---
+++ inc/lak/math.hpp
@@ -0,0 +1,13 @@
+#ifndef LAK_MATH_HPP
+#define LAK_MATH_HPP
+
+#include "lak/stdint.hpp"
+
+namespace lak
+{
+  bool close_to(double a, double b, uint32_t epsilon_count = 1);
+
+  bool close_to(float a, float b, uint32_t epsilon_count = 1);
+}
+
+#endif
---
+++ src/math.cpp
@@ -0,0 +1,20 @@
+#include "lak/math.hpp"
+
+#include <cmath>
+#include <limits>
+
+bool lak::close_to(double a, double b, uint32_t epsilon_count)
+{
+  const double diff = std::abs(a - b);
+  return (diff < DBL_MIN) ||
+         (diff <= (DBL_EPSILON * std::max(std::abs(a) + std::abs(b), 1.0) *
+                   epsilon_count));
+}
+
+bool lak::close_to(float a, float b, uint32_t epsilon_count)
+{
+  const float diff = std::abs(a - b);
+  return (diff < FLT_MIN) ||
+         (diff <= (FLT_EPSILON * std::max(std::abs(a) + std::abs(b), 1.0f) *
+                   epsilon_count));
+}
