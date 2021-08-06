f3ea7df8429f14cf8d2913f7c79039d4ab8aaa3a
LAK132 <lakridesagain@gmail.com>
2021-08-07 02:42:27 +0930

    add string_view test

---
+++ src/tests/string_view.cpp
@@ -0,0 +1,15 @@
+#include "lak/test.hpp"
+
+#include "lak/string_view.hpp"
+
+BEGIN_TEST(string_view)
+{
+  [[maybe_unused]] lak::astring_view astr     = "Hello, World!"_view;
+  [[maybe_unused]] lak::wstring_view wstr     = L"Hello, World!"_view;
+  [[maybe_unused]] lak::u8string_view u8str   = u8"Hello, World!"_view;
+  [[maybe_unused]] lak::u16string_view u16str = u"Hello, World!"_view;
+  [[maybe_unused]] lak::u32string_view u32str = U"Hello, World!"_view;
+
+  return 0;
+}
+END_TEST();
