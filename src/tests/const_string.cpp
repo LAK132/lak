4624b5a7c361076524081a17f214c7cab5cba4fc
LAK132 <lakridesagain@gmail.com>
2021-08-07 17:58:57 +0930

    more const_string tests

--- src/tests/const_string.cpp
+++ src/tests/const_string.cpp
@@ -9,9 +9,19 @@ lak::u8string_view template_test_function()
   return lak::string_view(STR.begin(), STR.size());
 }
 
-BEGIN_TEST(const_view)
+template<lak::u8const_string... STRS>
+lak::u8string template_test_function2()
 {
-  std::cout << template_test_function<u8"Hello, World!">();
+  return ((lak::u8string(STRS)) + ...);
+}
+
+BEGIN_TEST(const_string)
+{
+  lak::u8string_view view = template_test_function<u8"Hello, World!">();
+  lak::u8string str =
+    template_test_function2<u8"Hello", u8", ", u8"World", u8"!">();
+  ASSERT_EQUAL(view, u8"Hello, World!");
+  ASSERT_EQUAL(str, u8"Hello, World!");
   return 0;
 }
 END_TEST()
