20e5e2a6891c7329ce05456af7711eaf7b9ab540
LAK132 <lakridesagain@gmail.com>
2021-10-22 23:30:50 +1030

    add architecture macros

--- inc/lak/compiler.hpp
+++ inc/lak/compiler.hpp
@@ -47,14 +47,69 @@
 #	define packed_struct struct [[gnu::packed]]
 #	define DEBUG_BREAK() asm("int $3")
 #	define DLL_EXPORT    [[gnu::dllexport]]
+#	if defined(__x86_64__)
+#		define LAK_ARCH_X86_64
+#	endif
+#	if defined(__i386__)
+#		define LAK_ARCH_X86
+#	endif
+#	if defined(__ia64__)
+#		define LAK_ARCH_IA64
+#	endif
+#	if defined(__arm__)
+#		define LAK_ARCH_ARM
+#	endif
+#	if defined(__aarch64__)
+#		define LAK_ARCH_ARM64
+#	endif
 #elif defined(LAK_COMPILER_MSVC)
 #	include <intrin.h>
 #	define force_inline  inline __forceinline
 #	define packed_struct __pragma(pack(1)) struct
 #	define DEBUG_BREAK() __debugbreak()
 #	define DLL_EXPORT    __declspec(dllexport)
+#	if defined(_M_X64) || defined(_M_AMD64)
+#		define LAK_ARCH_X86_64
+#	endif
+#	if defined(_M_IX86)
+#		define LAK_ARCH_X86
+#	endif
+#	if defined(_M_IA64)
+#		define LAK_ARCH_IA64
+#	endif
+#	if defined(_M_ARM)
+#		define LAK_ARCH_ARM
+#	endif
+#	if defined(_M_ARM64)
+#		define LAK_ARCH_ARM64
+#	endif
+#	if defined(__AVX__)
+#		define LAK_ARCH_AVX
+#	endif
+#	if defined(__AVX2__)
+#		define LAK_ARCH_AVX2
+#	endif
+#	if defined(__AVX512__)
+#		define LAK_ARCH_AVX512
+#	endif
 #else
 #	error "Compiler not supported"
+#endif
+
+#if defined(LAK_ARCH_X86_64)
+#	define LAK_ARCH x86_64
+#endif
+#if defined(LAK_ARCH_X86)
+#	define LAK_ARCH x86
+#endif
+#if defined(LAK_ARCH_IA64)
+#	define LAK_ARCH ia64
+#endif
+#if defined(LAK_ARCH_ARM)
+#	define LAK_ARCH arm
+#endif
+#if defined(LAK_ARCH_ARM64)
+#	define LAK_ARCH aarch64
 #endif
 
 #endif
