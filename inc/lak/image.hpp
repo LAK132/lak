e144f6bea32b1a6db8b3edc848ba5aaa49e0f803
LAK132 <lakridesagain@gmail.com>
2021-12-16 16:00:31 +1030

    rename image from functions

--- inc/lak/image.hpp
+++ inc/lak/image.hpp
@@ -429,15 +429,16 @@ namespace lak
 	using image3_t = image<color3_t>;
 	using image4_t = image<color4_t>;
 
-	[[maybe_unused]] inline image3_t ImageFromRGB24(byte_t *pixels, vec2s_t size)
+	[[maybe_unused]] inline image3_t image_from_rgb24(const byte_t *pixels,
+	                                                  vec2s_t size)
 	{
 		return image3_t::from_bytes(
 		         lak::span<const byte_t>(pixels, size.x * size.y * 3), size)
 		  .unwrap();
 	}
 
-	[[maybe_unused]] inline image4_t ImageFromRGBA32(byte_t *pixels,
-	                                                 vec2s_t size)
+	[[maybe_unused]] inline image4_t image_from_rgba32(const byte_t *pixels,
+	                                                   vec2s_t size)
 	{
 		return image4_t::from_bytes(
 		         lak::span<const byte_t>(pixels, size.x * size.y * 4), size)
