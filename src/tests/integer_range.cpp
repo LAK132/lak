e5330f8f4bc49c7249abdf4190b32c7f907a2044
LAK132 <lakridesagain@gmail.com>
2021-12-16 16:03:18 +1030

    fix signed/unsigned mismatch

--- src/tests/integer_range.cpp
+++ src/tests/integer_range.cpp
@@ -5,23 +5,23 @@
 BEGIN_TEST(integer_range)
 {
 	{
-		size_t i = 10;
-		for (size_t x : lak::size_range::from_to(10, 100))
+		size_t i = 10U;
+		for (size_t x : lak::size_range::from_to(10U, 100U))
 		{
 			ASSERT_EQUAL(i, x);
 			++i;
 		}
-		ASSERT_EQUAL(i, 100);
+		ASSERT_EQUAL(i, 100U);
 	}
 
 	{
-		size_t i = 10;
-		for (size_t x : lak::size_range::from_count(10, 100))
+		size_t i = 10U;
+		for (size_t x : lak::size_range::from_count(10U, 100U))
 		{
 			ASSERT_EQUAL(i, x);
 			++i;
 		}
-		ASSERT_EQUAL(i, 110);
+		ASSERT_EQUAL(i, 110U);
 	}
 
 	return 0;
