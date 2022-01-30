782daaa06a6986739e57b8bcbd97c9735aac067e
LAK132 <lakridesagain@gmail.com>
2022-01-30 21:18:16 +1030

    brace init defer objects

--- inc/lak/defer.hpp
+++ inc/lak/defer.hpp
@@ -25,11 +25,11 @@ namespace lak
 }
 
 #define DEFER(...)                                                            \
-	auto UNIQUIFY(DEFER_OBJECT_) = lak::defer([&]() { __VA_ARGS__; })
+	auto UNIQUIFY(DEFER_OBJECT_){lak::defer([&]() { __VA_ARGS__; })};
 #define DEFER_RESET(OBJ)                                                      \
-	auto UNIQUIFY(DEFER_RESET_OBJECT_) = OBJ;                                   \
+	auto UNIQUIFY(DEFER_RESET_OBJECT_){OBJ};                                    \
 	DEFER(OBJ = lak::move(UNIQUIFY(DEFER_RESET_OBJECT_)));
 #define DEFER_CALL(FUNC, ...)                                                 \
-	auto UNIQUIFY(DEFER_OBJECT_) = lak::defer(std::bind(FUNC, __VA_ARGS__))
+	auto UNIQUIFY(DEFER_OBJECT_){lak::defer(std::bind(FUNC, __VA_ARGS__))};
 
 #endif
