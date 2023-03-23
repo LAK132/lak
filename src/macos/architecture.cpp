73dc324d87809e8f992c8224c1ad5e5d5f701218
LAK132 <lakridesagain@gmail.com>
2023-03-23 19:36:53 +1030

    memmanip fixes for macos

---
+++ src/macos/architecture.cpp
@@ -0,0 +1,7 @@
+#include "lak/architecture.hpp"
+
+lak::result<lak::architecture> lak::host_os_architecture()
+{
+	ASSERT_NYI();
+	// return lak::host_cpu_architecture();
+}
--- src/posix/memmanip.cpp
+++ src/posix/memmanip.cpp
@@ -19,7 +19,10 @@ lak::page_result_t<lak::span<void>> lak::page_reserve(size_t size,
 	size = lak::round_to_page_multiple(size, page_size_out);
 
 	auto ptr_to_span = [size](void *ptr) { return lak::span<void>(ptr, size); };
-
+#ifdef LAK_OS_APPLE
+	return lak::posix::mmap(
+	         nullptr, size, PROT_NONE, MAP_PRIVATE | MAP_ANON, -1, 0)
+#else
 	return lak::posix::open("/dev/zero", O_RDWR)
 	  .and_then(
 	    [size](int fd)
@@ -27,8 +30,26 @@ lak::page_result_t<lak::span<void>> lak::page_reserve(size_t size,
 		    return lak::posix::mmap(
 		      nullptr, size, PROT_NONE, MAP_PRIVATE | MAP_FILE, fd, 0);
 	    })
+#endif
 	  .map_err([](auto &&) -> lak::page_error { return {}; })
 	  .map(ptr_to_span);
+}
+
+lak::page_result_t<> lak::page_decommit(lak::span<void> pages)
+{
+	// https://bugzilla.mozilla.org/show_bug.cgi?id=670596
+	return lak::posix::mprotect(pages.data(), pages.size(), PROT_NONE)
+	  .and_then(
+	    [&](auto &&)
+	    {
+#ifdef LAK_OS_APPLE
+		    return lak::posix::madvise(pages.data(), pages.size(), MADV_FREE);
+#else
+		    return lak::posix::madvise(pages.data(), pages.size(), MADV_DONTNEED);
+#endif
+	    })
+	  .map_err([](auto &&) -> lak::page_error { return {}; })
+	  .map([](auto &&) -> lak::monostate { return {}; });
 }
 
 lak::page_result_t<> lak::page_commit(lak::span<void> pages)
