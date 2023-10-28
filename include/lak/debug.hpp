c3b4474a064ed2bb863a431f3e3d1ea675b91901
LAK132 <lakridesagain@gmail.com>
2023-10-29 01:27:35 +1030

    cleaner scoped checkpoints

--- include/lak/debug.hpp
+++ include/lak/debug.hpp
@@ -98,9 +98,13 @@
 		lak::scoped_indenter UNIQUIFY(SCOPED_INDENTOR_)(                          \
 		  lak::streamify(__VA_ARGS__ __VA_OPT__(, " ") DEBUG_LINE_FILE));
 #	define FUNCTION_CHECKPOINT(...)                                            \
-		SCOPED_CHECKPOINT(__VA_ARGS__ __VA_OPT__(, ) __func__)
+		SCOPED_CHECKPOINT(__func__, "(" __VA_OPT__(, ) __VA_ARGS__, ")")
 #	define MEMBER_FUNCTION_CHECKPOINT(...)                                     \
-		FUNCTION_CHECKPOINT(__VA_ARGS__ __VA_OPT__(, ) typeid(*this).name(), "::");
+		SCOPED_CHECKPOINT(typeid(*this).name(),                                   \
+		                  "::",                                                   \
+		                  __func__,                                               \
+		                  "(" __VA_OPT__(, ) __VA_ARGS__,                         \
+		                  ")")
 #	define DEBUG(...)                                                          \
 		lak::debugger.std_out(TO_U8STRING(DEBUG_DEBUG_LINE_FILE),                 \
 		                      lak::streamify(__VA_ARGS__, "\n"));
