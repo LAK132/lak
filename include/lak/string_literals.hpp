6b4e00eba5402b0a4d9e9993bc30047197ecf9fe
LAK132 <lakridesagain@gmail.com>
2023-11-12 20:46:57 +1030

    rework ""_magic_*

--- include/lak/string_literals.hpp
+++ include/lak/string_literals.hpp
@@ -2,6 +2,7 @@
 #define LAK_STRING_LITERALS_HPP
 
 #include "lak/const_string.hpp"
+#include "lak/math.hpp"
 #include "lak/span.hpp"
 #include "lak/string_view.hpp"
 
@@ -76,7 +77,7 @@ inline consteval lak::span<const char32_t> operator"" _span(
 template<lak::u8const_string STR>
 requires(STR.size() == 1U || STR.size() == 2U || STR.size() == 4U ||
          STR.size() == 8U)
-inline consteval auto operator"" _magic()
+inline consteval auto operator"" _magic_ne()
 {
 	using result_type = lak::conditional_t<
 	  STR.size() == 1U,
@@ -90,5 +91,59 @@ inline consteval auto operator"" _magic()
 
 	return std::bit_cast<result_type>(STR);
 }
+
+#if defined(LAK_LITTLE_ENDIAN)
+static_assert(u8"ABCD"_magic_ne == 0x44'43'42'41);
+#elif defined(LAK_BIG_ENDIAN)
+static_assert(u8"ABCD"_magic_ne == 0x41'42'43'44);
+#endif
+
+template<lak::u8const_string STR>
+requires(STR.size() == 1U || STR.size() == 2U || STR.size() == 4U ||
+         STR.size() == 8U)
+inline consteval auto operator"" _magic_le()
+{
+	using result_type = lak::conditional_t<
+	  STR.size() == 1U,
+	  uint8_t,
+	  lak::conditional_t<
+	    STR.size() == 2U,
+	    uint16_t,
+	    lak::conditional_t<STR.size() == 4U, uint32_t, uint64_t>>>;
+
+	static_assert(sizeof(STR) == sizeof(result_type));
+
+	result_type result = 0U;
+	for (size_t i = 0U; i < STR.size(); ++i)
+		result |= result_type(STR[i]) << (i * sizeof(STR[0U]) * CHAR_BIT);
+	return result;
+}
+
+static_assert(u8"ABCD"_magic_le == 0x44'43'42'41);
+
+template<lak::u8const_string STR>
+requires(STR.size() == 1U || STR.size() == 2U || STR.size() == 4U ||
+         STR.size() == 8U)
+inline consteval auto operator"" _magic_be()
+{
+	using result_type = lak::conditional_t<
+	  STR.size() == 1U,
+	  uint8_t,
+	  lak::conditional_t<
+	    STR.size() == 2U,
+	    uint16_t,
+	    lak::conditional_t<STR.size() == 4U, uint32_t, uint64_t>>>;
+
+	static_assert(sizeof(STR) == sizeof(result_type));
+
+	result_type result = 0U;
+	for (size_t i = 0U; i < STR.size(); ++i)
+		result |= result_type(STR[i])
+		          << lak::integer_range_reverse<size_t>(i, STR.size()) *
+		               sizeof(STR[0U]) * CHAR_BIT;
+	return result;
+}
+
+static_assert(u8"ABCD"_magic_be == 0x41'42'43'44);
 
 #endif
