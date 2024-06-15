5f637586fb63fe45d02d1ed795ffd946db97291d
LAK132 <lakridesagain@gmail.com>
2024-06-15 20:14:35 +0930

    add basic_window

---
+++ include/lak/imgui/basic_window.hpp
@@ -0,0 +1,59 @@
+#ifndef LAK_BASIC_WINDOW_HPP
+#define LAK_BASIC_WINDOW_HPP
+
+#include <lak/imgui/backend.hpp>
+#include <lak/imgui/widgets.hpp>
+
+#include <lak/opengl/texture.hpp>
+
+#include <lak/bit_reader.hpp>
+#include <lak/span_manip.hpp>
+#include <lak/string_literals.hpp>
+
+namespace lak
+{
+	template<typename DERIVED>
+	struct basic_window
+	{
+		static void menu_bar(float) {}
+
+		static void left_region(float) {}
+
+		static void right_region(float) {}
+
+		static void main_region(float frame_time)
+		{
+			const auto content_size{ImGui::GetContentRegionAvail()};
+
+			static float left_size  = content_size.x / 2;
+			static float right_size = content_size.x / 2;
+
+			lak::VertSplitter(left_size, right_size, content_size.x);
+
+			ImGui::BeginChild(
+			  "Left", {left_size, -1}, true, ImGuiWindowFlags_NoSavedSettings);
+			DERIVED::left_region(frame_time);
+			ImGui::EndChild();
+
+			ImGui::SameLine();
+
+			ImGui::BeginChild(
+			  "Right", {right_size, -1}, true, ImGuiWindowFlags_NoSavedSettings);
+			DERIVED::right_region(frame_time);
+			ImGui::EndChild();
+		}
+
+		static void draw(float frame_time)
+		{
+			if (ImGui::BeginMenuBar())
+			{
+				DERIVED::menu_bar(frame_time);
+				ImGui::EndMenuBar();
+			}
+
+			DERIVED::main_region(frame_time);
+		}
+	};
+}
+
+#endif
