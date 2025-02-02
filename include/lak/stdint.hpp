e8a69c757dc1c0e4dea73e463aad52d544f2eb55
LAK132 <lakridesagain@gmail.com>
2025-02-03 02:54:37 +1030

    separate macros for signed and unsigned integers

--- include/lak/stdint.hpp
+++ include/lak/stdint.hpp
@@ -22,15 +22,21 @@ enum struct byte_t : uint8_t
 static_assert(sizeof(f32_t) == 4);
 static_assert(sizeof(f64_t) == 8);
 
-#define LAK_FOREACH_INTEGER(MACRO, ...)                                       \
-	MACRO(uint8_t, u8, __VA_ARGS__)                                             \
+#define LAK_FOREACH_SIGNED_INTEGER(MACRO, ...)                                \
 	MACRO(int8_t, s8, __VA_ARGS__)                                              \
-	MACRO(uint16_t, u16, __VA_ARGS__)                                           \
 	MACRO(int16_t, s16, __VA_ARGS__)                                            \
-	MACRO(uint32_t, u32, __VA_ARGS__)                                           \
 	MACRO(int32_t, s32, __VA_ARGS__)                                            \
-	MACRO(uint64_t, u64, __VA_ARGS__)                                           \
 	MACRO(int64_t, s64, __VA_ARGS__)
+
+#define LAK_FOREACH_UNSIGNED_INTEGER(MACRO, ...)                              \
+	MACRO(uint8_t, u8, __VA_ARGS__)                                             \
+	MACRO(uint16_t, u16, __VA_ARGS__)                                           \
+	MACRO(uint32_t, u32, __VA_ARGS__)                                           \
+	MACRO(uint64_t, u64, __VA_ARGS__)
+
+#define LAK_FOREACH_INTEGER(MACRO, ...)                                       \
+	LAK_FOREACH_SIGNED_INTEGER(MACRO, __VA_ARGS__)                              \
+	LAK_FOREACH_UNSIGNED_INTEGER(MACRO, __VA_ARGS__)
 
 #define LAK_FOREACH_FLOAT(MACRO, ...)                                         \
 	MACRO(f32_t, f32, __VA_ARGS__)                                              \
