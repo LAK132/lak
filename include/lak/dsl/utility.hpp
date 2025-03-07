822cd1fd3d8b97b81bc0da38c5cacfd8295c8230
LAK132 <lakridesagain@gmail.com>
2025-03-08 01:48:56 +1030

    refactor uint parsing code

--- include/lak/dsl/utility.hpp
+++ include/lak/dsl/utility.hpp
@@ -28,19 +28,17 @@ namespace lak
 
 		inline constexpr auto number_sign = lak::dsl::one_of_chars<U'+', U'-'>;
 
-		inline constexpr auto bin_digit  = lak::dsl::char_range<U'0', U'1'>;
-		inline constexpr auto bin_number = +lak::dsl::bin_digit;
-		template<typename UINT>
-		inline constexpr auto parsed_bin_uint = lak::dsl::transform<
-		  lak::dsl::bin_number,
+		template<lak::dsl::parser auto PAR, lak::numeric_base BASE, typename UINT>
+		inline constexpr auto parsed_uint = lak::dsl::transform<
+		  PAR,
 		  [](lak::u8string_view num)
 		  {
-			  return lak::string_to_uintmax(num, lak::numeric_base::dec)
+			  return lak::string_to_uintmax(num, BASE)
 			    .map_err(
 			      [](lak::string_to_numeric_error)
 			      {
 				      return lak::dsl::parse_error{
-				        .message = u8"error converting bin string to uintmax_t"};
+				        .message = u8"error converting string to uintmax_t"};
 			      })
 			    .and_then(
 			      [](uintmax_t v) -> lak::result<UINT, lak::dsl::parse_error>
@@ -53,39 +51,25 @@ namespace lak
 			      });
 		  }>;
 
-		inline constexpr auto oct_digit       = lak::dsl::char_range<U'0', U'7'>;
-		inline constexpr auto oct_number      = +lak::dsl::oct_digit;
-		inline constexpr auto parsed_oct_uint = lak::dsl::transform<
-		  lak::dsl::oct_number,
-		  [](lak::u8string_view num)
-		  { return lak::string_to_uintmax(num, lak::numeric_base::oct); }>;
+		inline constexpr auto bin_digit  = lak::dsl::char_range<U'0', U'1'>;
+		inline constexpr auto bin_number = +lak::dsl::bin_digit;
+		template<typename UINT>
+		inline constexpr auto parsed_bin_uint = lak::dsl::
+		  parsed_uint<lak::dsl::bin_number, lak::numeric_base::bin, UINT>;
+
+		inline constexpr auto oct_digit  = lak::dsl::char_range<U'0', U'7'>;
+		inline constexpr auto oct_number = +lak::dsl::oct_digit;
+		template<typename UINT>
+		inline constexpr auto parsed_oct_uint = lak::dsl::
+		  parsed_uint<lak::dsl::oct_number, lak::numeric_base::oct, UINT>;
 
 		inline constexpr auto dec_digit  = lak::dsl::char_range<U'0', U'9'>;
 		inline constexpr auto dec_number = +lak::dsl::dec_digit;
 		inline constexpr auto signed_dec_number =
 		  lak::dsl::optional<lak::dsl::number_sign> + lak::dsl::dec_number;
 		template<typename UINT>
-		inline constexpr auto parsed_dec_uint = lak::dsl::transform<
-		  lak::dsl::dec_number,
-		  [](lak::u8string_view num)
-		  {
-			  return lak::string_to_uintmax(num, lak::numeric_base::dec)
-			    .map_err(
-			      [](lak::string_to_numeric_error)
-			      {
-				      return lak::dsl::parse_error{
-				        .message = u8"error converting dec string to uintmax_t"};
-			      })
-			    .and_then(
-			      [](uintmax_t v) -> lak::result<UINT, lak::dsl::parse_error>
-			      {
-				      if (v > std::numeric_limits<UINT>::max())
-					      return lak::err_t{lak::dsl::parse_error{
-					        .message = u8"parsed value out of range"}};
-				      else
-					      return lak::ok_t{UINT(v)};
-			      });
-		  }>;
+		inline constexpr auto parsed_dec_uint = lak::dsl::
+		  parsed_uint<lak::dsl::dec_number, lak::numeric_base::dec, UINT>;
 		template<typename SINT>
 		inline constexpr auto parsed_dec_int = lak::dsl::transform<
 		  lak::dsl::signed_dec_number,
@@ -115,27 +99,8 @@ namespace lak
 		                                  lak::dsl::char_range<U'A', U'F'>;
 		inline constexpr auto hex_number = +lak::dsl::hex_digit;
 		template<typename UINT>
-		inline constexpr auto parsed_hex_uint = lak::dsl::transform<
-		  lak::dsl::hex_number,
-		  [](lak::u8string_view num)
-		  {
-			  return lak::string_to_uintmax(num, lak::numeric_base::hex)
-			    .map_err(
-			      [](lak::string_to_numeric_error)
-			      {
-				      return lak::dsl::parse_error{
-				        .message = u8"error converting hex string to uintmax_t"};
-			      })
-			    .and_then(
-			      [](uintmax_t v) -> lak::result<UINT, lak::dsl::parse_error>
-			      {
-				      if (v > std::numeric_limits<UINT>::max())
-					      return lak::err_t{lak::dsl::parse_error{
-					        .message = u8"parsed value out of range"}};
-				      else
-					      return lak::ok_t{UINT(v)};
-			      });
-		  }>;
+		inline constexpr auto parsed_hex_uint = lak::dsl::
+		  parsed_uint<lak::dsl::hex_number, lak::numeric_base::hex, UINT>;
 
 		template<lak::dsl::pure_match_parser auto frac_separator,
 		         lak::dsl::pure_match_parser auto exp_separator>
