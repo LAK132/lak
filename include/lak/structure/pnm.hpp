19b75af06d5c4d8896efddd1b77ce0f793f945a9
LAK132 <lakridesagain@gmail.com>
2025-07-07 22:35:35 +0930

    temp disable pnm static assert on msvc

--- include/lak/structure/pnm.hpp
+++ include/lak/structure/pnm.hpp
@@ -86,8 +86,11 @@ namespace lak
 	}
 }
 
+#ifndef LAK_COMPILER_MSVC
+// :TODO: MSVC is ICEing trying to compile this.
 static_assert(
   lak::concepts::from_bytes_readable<lak::pnm::pnm, lak::endian::native>);
+#endif
 
 #include "pnm.inl"
 
