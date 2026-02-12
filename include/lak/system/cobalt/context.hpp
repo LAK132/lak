76fb86d7e2795e9497061aa7fd0ddd66268c9f1b
LAK132 <lakridesagain@gmail.com>
2026-02-13 00:44:10 +1030

    add format traits for some cobalt enums

--- include/lak/system/cobalt/context.hpp
+++ include/lak/system/cobalt/context.hpp
@@ -3,7 +3,10 @@
 
 #include "lak/system/windowing/window.hpp"
 
+#include "lak/format.hpp"
+
 #include <RendererInterface/RendererInterface.pkg>
+#include <RendererSupport/VendorName.h>
 
 namespace lak
 {
@@ -33,5 +36,48 @@ namespace lak
 		};
 	}
 }
+
+template<typename CHAR>
+struct lak::format_traits<::cobalt::graphics::IRendererInfo::ApiFamily, CHAR>
+{
+	static lak::string<CHAR> to_string(
+	  ::cobalt::graphics::IRendererInfo::ApiFamily family)
+	{
+		switch (family)
+		{
+			case ::cobalt::graphics::IRendererInfo::ApiFamily::OpenGL:
+				return lak::strconv<CHAR>("OpenGL"_view);
+			case ::cobalt::graphics::IRendererInfo::ApiFamily::OpenGLES:
+				return lak::strconv<CHAR>("OpenGLES"_view);
+			case ::cobalt::graphics::IRendererInfo::ApiFamily::Direct3D:
+				return lak::strconv<CHAR>("Direct3D"_view);
+			case ::cobalt::graphics::IRendererInfo::ApiFamily::Vulkan:
+				return lak::strconv<CHAR>("Vulkan"_view);
+			case ::cobalt::graphics::IRendererInfo::ApiFamily::Metal:
+				return lak::strconv<CHAR>("Metal"_view);
+			default: ASSERT_UNREACHABLE(); return lak::strconv<CHAR>("Unknown"_view);
+		}
+	}
+};
+
+template<typename CHAR>
+struct lak::format_traits<::cobalt::graphics::IRendererInfo::ApiVersion, CHAR>
+{
+	static lak::string<CHAR> to_string(
+	  ::cobalt::graphics::IRendererInfo::ApiVersion version)
+	{
+		return lak::fmt<CHAR, "{}.{}">(version.major, version.minor);
+	}
+};
+
+template<typename CHAR>
+struct lak::format_traits<::cobalt::graphics::IGraphicsDevice::Vendor, CHAR>
+{
+	static lak::string<CHAR> to_string(
+	  ::cobalt::graphics::IGraphicsDevice::Vendor vendor)
+	{
+		return lak::strconv<CHAR>(::cobalt::graphics::VendorToVendorName(vendor));
+	}
+};
 
 #endif
