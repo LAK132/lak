048c3a8cdf9b0111f5f30605f9f0302f2aac830e
LAK132 <lakridesagain@gmail.com>
2026-05-03 15:28:26 +0930

    use format instead of streamify for errno_error

--- include/lak/errno_result.hpp
+++ include/lak/errno_result.hpp
@@ -1,12 +1,11 @@
 #ifndef LAK_ERRNO_RESULT_HPP
 #define LAK_ERRNO_RESULT_HPP
 
+#include "lak/format.hpp"
 #include "lak/result.hpp"
 #include "lak/strconv.hpp"
-#include "lak/streamify.hpp"
 
 #include <cerrno>
-#include <ostream>
 #include <system_error>
 
 namespace lak
@@ -17,21 +16,24 @@ namespace lak
 
 		static errno_error last_error() { return {errno}; }
 
-		inline lak::astring to_string() const
+		template<typename CHAR = char>
+		inline lak::string<CHAR> to_string() const
 		{
-			return lak::to_astring(lak::streamify(
-			  value,
-			  ": ",
-			  std::error_code(value, std::generic_category()).message()));
+			auto msg = std::error_code(value, std::generic_category()).message();
+			return lak::fmt<CHAR, "{} ({})">(lak::string_view(msg), value);
 		}
+
+		inline lak::u8string to_u8string() const { return to_string<char8_t>(); }
 	};
 
 	template<typename CHAR>
-	std::basic_ostream<CHAR> &operator<<(std::basic_ostream<CHAR> &strm,
-	                                     const lak::errno_error &err)
+	struct format_traits<lak::errno_error, CHAR>
 	{
-		return strm << lak::string_view{err.to_string()};
-	}
+		static constexpr lak::string<CHAR> to_string(const lak::errno_error &err)
+		{
+			return err.template to_string<CHAR>();
+		}
+	};
 
 	template<typename T = lak::monostate>
 	using errno_result = lak::result<T, lak::errno_error>;
