8e7a12e674c6928f787d83fda58ce9949eb24508
LAK132 <lakridesagain@gmail.com>
2026-07-03 04:16:08 +0930

    clang-format

--- src/system/windowing/sdl/platform.cpp
+++ src/system/windowing/sdl/platform.cpp
@@ -12,9 +12,9 @@ bool lak::platform_init()
 #ifdef LAK_OS_APPLE
 #	ifdef VK_DRIVER_FILE
 	{
-		auto files = (lak::exe_path().parent_path()/VK_DRIVER_FILE).string();
+		auto files = (lak::exe_path().parent_path() / VK_DRIVER_FILE).string();
 #		ifdef VK_DRIVER_PREFIX
-		files += ":" + (lak::fs::path(VK_DRIVER_PREFIX)/VK_DRIVER_FILE).string();
+		files += ":" + (lak::fs::path(VK_DRIVER_PREFIX) / VK_DRIVER_FILE).string();
 #		endif
 		DEBUG_EXPR(files);
 		ASSERT_EQUAL(setenv("VK_DRIVER_FILES", files.c_str(), 1), 0);
