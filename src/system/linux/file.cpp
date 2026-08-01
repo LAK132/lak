82c2f5a66de84b4733a394604790e930737fd7be
LAK132 <lakridesagain@gmail.com>
2026-08-01 23:10:48 +0930

    fix linux exe_path

--- src/system/linux/file.cpp
+++ src/system/linux/file.cpp
@@ -2,48 +2,54 @@
 
 #include "lak/array.hpp"
 #include "lak/debug.hpp"
+#include "lak/errno_result.hpp"
 
 #include <sys/stat.h>
 #include <unistd.h>
 
 lak::fs::path lak::exe_path()
 {
-	const auto *proc_str = "/proc/self/exe";
+	const char *proc_str = "/proc/self/exe";
 	struct stat sb;
 	lak::array<char> path;
 
-	size_t read = 0;
-
-	do
+	if (lstat(proc_str, &sb) == -1)
 	{
-		if (lstat(proc_str, &sb) == -1)
-		{
-			ERROR("Failed to lstat ", proc_str);
-			return {};
-		}
+		WARNING(
+		  "Failed to lstat ", proc_str, " (", lak::errno_error::last_error(), ")");
+		path.resize(PATH_MAX);
+	}
+	else if (sb.st_size < 0)
+	{
+		WARNING("Bad stat size ", sb.st_size);
+		path.resize(PATH_MAX);
+	}
+	else if (sb.st_size == 0)
+	{
+		path.resize(PATH_MAX);
+	}
+	else
+	{
+		path.resize(static_cast<size_t>(sb.st_size) + 1U);
+	}
 
-		if (sb.st_size < 0)
-		{
-			ERROR("Bad stat size ", sb.st_size);
-			return {};
-		}
+	size_t read = lak::dynamic_extent;
 
-		size_t st_size = static_cast<size_t>(sb.st_size);
-
-		if (st_size > path.size()) path.resize(st_size + 1);
-
+	for (; read >= path.size(); path.resize((path.size() * 2U) + 1U))
+	{
 		ssize_t _read = readlink(proc_str, path.data(), path.size());
 
 		if (_read < 0)
 		{
-			ERROR("Failed to read link (", _read, ")");
+			ERROR("Failed to read link (", lak::errno_error::last_error(), ")");
 			return {};
 		}
 
 		read = static_cast<size_t>(_read);
-	} while (read > static_cast<size_t>(sb.st_size));
+	}
 
-	if (read + 1 < path.size()) path[read + 1] = 0;
+	ASSERT_GREATER(path.size(), read);
+	path[read] = 0;
 
 	return lak::fs::path(path.data());
 }
