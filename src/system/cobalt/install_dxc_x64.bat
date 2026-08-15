319f6e0abcef37d8d5847584750faca3f41acefd
LAK132 <lakridesagain@gmail.com>
2026-08-16 04:56:27 +0930

    hopefully fix releases for good

--- src/system/cobalt/install_dxc_x64.bat
+++ src/system/cobalt/install_dxc_x64.bat
@@ -1,7 +1,2 @@
-if "%DESTDIR%"=="" (
-if not exist "%MESON_INSTALL_PREFIX%\bin" mkdir "%MESON_INSTALL_PREFIX%\bin"
-copy "%MESON_SOURCE_ROOT%\subprojects\Cobalt\External\Cache\DirectXShaderCompilerOfficial\bin\x64\dxcompiler.dll" "%MESON_INSTALL_PREFIX%\bin\"
-) else (
-if not exist "%MESON_INSTALL_PREFIX%\bin" mkdir "%DESTDIR%\%MESON_INSTALL_PREFIX%\bin"
-copy "%MESON_SOURCE_ROOT%\subprojects\Cobalt\External\Cache\DirectXShaderCompilerOfficial\bin\x64\dxcompiler.dll" "%DESTDIR%\%MESON_INSTALL_PREFIX%\bin\"
-)
+if not exist "%MESON_INSTALL_DESTDIR_PREFIX%\bin" mkdir "%MESON_INSTALL_DESTDIR_PREFIX%\bin"
+copy "%MESON_SOURCE_ROOT%\subprojects\Cobalt\External\Cache\DirectXShaderCompilerOfficial\bin\x64\dxcompiler.dll" "%MESON_INSTALL_DESTDIR_PREFIX%\bin\"
--- src/system/cobalt/install_moltenvk.sh
+++ src/system/cobalt/install_moltenvk.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/vulkan/icd.d"
-cp "${MESON_BUILD_ROOT}/subprojects/Cobalt/__CMake_build/_deps/vulkan-sdk-install/macOS/share/vulkan/icd.d/MoltenVK_icd.json" "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/vulkan/icd.d/"
+mkdir -p "${MESON_INSTALL_DESTDIR_PREFIX}/share/vulkan/icd.d"
+cp "${MESON_BUILD_ROOT}/subprojects/Cobalt/__CMake_build/_deps/vulkan-sdk-install/macOS/share/vulkan/icd.d/MoltenVK_icd.json" "${MESON_INSTALL_DESTDIR_PREFIX}/share/vulkan/icd.d/"
 
-mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib"
-cp "${MESON_BUILD_ROOT}/subprojects/Cobalt/__CMake_build/_deps/vulkan-sdk-install/macOS/lib/libMoltenVK.dylib" "${DESTDIR}/${MESON_INSTALL_PREFIX}/lib/"
+mkdir -p "${MESON_INSTALL_DESTDIR_PREFIX}/lib"
+cp "${MESON_BUILD_ROOT}/subprojects/Cobalt/__CMake_build/_deps/vulkan-sdk-install/macOS/lib/libMoltenVK.dylib" "${MESON_INSTALL_DESTDIR_PREFIX}/lib/"
