51695f0288cb17ec3be15050daa51a0348b040cc
LAK132 <lakridesagain@gmail.com>
2026-08-16 23:33:38 +0930

    do a debug release as well

--- tools/workflow-matrix/release.cpp
+++ tools/workflow-matrix/release.cpp
@@ -4,42 +4,43 @@ int main()
 {
 	using namespace std::literals::string_literals;
 
+	std::string windows_setup =
+	  "--vsenv "
+	  "-Dcpp_args=\\\"-D_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR\\\" "
+	  "-Dlak_bounds_checks=always "
+	  "-Dlak_enable_examples=true "
+	  "-Dlak_enable_tests=true "
+	  "-Dlak_enable_windowing=true "
+	  "-Dlak_enable_imgui=true "
+	  "-Dlak_enable_stb=true "
+	  "-Dlak_enable_stb_image=true "
+	  "-Dlak_enable_stb_image_write=true "
+	  "-Dlak_use_native_file_dialog=true "
+	  "-Dlak_backend=win32 "
+	  "\\\"-Dlak_renderer=softrender,opengl,cobalt\\\" "
+	  "\\\"-Dcobalt_renderer=OpenGL3,OpenGL4,Vulkan,Direct3D11,Direct3D12\\\""s;
+	std::string windows_run =
+	  "install/bin/lak_test --help ; "
+	  "install/bin/lak_test --testall"s;
+
 	std::vector<matrix_entry_t> entries = {
 	  {
-	    .os      = {.runner = "windows-2025-vs2026"s,
-	                .system = "windows"s,
-	                .arch   = "x64"s},
-	    .targets = {{
-	      .setups =
+	    .os = {.runner = "windows-2025-vs2026"s,
+	           .system = "windows"s,
+	           .arch   = "x64"s},
+	    .targets =
+	      {
 	        {
-	          "--vsenv "
-	          "--buildtype=release "
-	          "-Dcpp_args=\\\"-D_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR\\\" "
-	          "-Dlak_bounds_checks=always "
-	          "-Dlak_enable_examples=true "
-	          "-Dlak_enable_tests=true "
-	          "-Dlak_enable_windowing=true "
-	          "-Dlak_enable_imgui=true "
-	          "-Dlak_enable_stb=true "
-	          "-Dlak_enable_stb_image=true "
-	          "-Dlak_enable_stb_image_write=true "
-	          "-Dlak_use_native_file_dialog=true "
-	          "-Dlak_backend=win32 "
-	          "\\\"-Dlak_renderer=softrender,opengl,cobalt\\\" "
-	          "\\\"-Dcobalt_renderer=OpenGL3,OpenGL4,Vulkan,Direct3D11,Direct3D12\\\""s,
+	          .setups = {windows_setup},
+	          .target = "debug"s,
+	          .run    = windows_run,
 	        },
-	      .target = "lak_test "
-	                "asc2csv "
-	                "ebnf2cpp "
-	                "lisk-repl "
-	                "mdc2png "
-	                "basic_program "
-	                "hello-cobalt "
-	                "mdc-view "
-	                "nbt-view"s,
-	      .run    = "install/bin/lak_test --help ; "
-	                "install/bin/lak_test --testall"s,
-	    }},
+	        {
+	          .setups = {windows_setup},
+	          .target = "release"s,
+	          .run    = windows_run,
+	        },
+	      },
 	  },
 	};
 
