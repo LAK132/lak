9ba6120464f36e328ead610ab4804aa9b75b7dca
LAK132 <lakridesagain@gmail.com>
2026-08-29 14:54:52 +0930

    draw CMY points on chromaticity plot

--- src/imgui/cie.cpp
+++ src/imgui/cie.cpp
@@ -55,17 +55,43 @@ static bool _chromaticity_plot(const char *name,
 			ImPlot::PlotLineG(
 			  "1964 10deg", cie_getter, &data_1964, int(data_1964.size()));
 
+			const auto mat = [&]()
+			{
+				if constexpr (lak::is_same_v<lak::remove_const_t<PRIMARIES>,
+				                             lak::col::cie::xy_primaries>)
+					return lak::col::cie::to_xyY(*primaries).linear_to_xyY();
+				else
+					return primaries->linear_to_xyY();
+			}();
+
+			const auto r_co = mat(lak::vec3f_t(1.f, 0.f, 0.f));
+			const auto g_co = mat(lak::vec3f_t(0.f, 1.f, 0.f));
+			const auto b_co = mat(lak::vec3f_t(0.f, 0.f, 1.f));
+			const auto w_co = mat(lak::vec3f_t(1.f, 1.f, 1.f));
+
+			const auto c_co = mat(lak::vec3f_t(0.f, 1.f, 1.f));
+			const auto m_co = mat(lak::vec3f_t(1.f, 0.f, 1.f));
+			const auto y_co = mat(lak::vec3f_t(1.f, 1.f, 0.f));
+
 			double x_coords[] = {
-			  primaries->r.x,
-			  primaries->g.x,
-			  primaries->b.x,
-			  primaries->w.x,
+			  r_co.x,
+			  g_co.x,
+			  b_co.x,
+			  w_co.x,
+
+			  c_co.x,
+			  m_co.x,
+			  y_co.x,
 			};
 			double y_coords[] = {
-			  primaries->r.y,
-			  primaries->g.y,
-			  primaries->b.y,
-			  primaries->w.y,
+			  r_co.y,
+			  g_co.y,
+			  b_co.y,
+			  w_co.y,
+
+			  c_co.y,
+			  m_co.y,
+			  y_co.y,
 			};
 
 			ImPlot::PlotLine(lak::fmt<"{}##lines">(name).c_str(),
@@ -75,28 +101,41 @@ static bool _chromaticity_plot(const char *name,
 			                 ImPlotLineFlags_Loop);
 
 			auto plot_point =
-			  [&](
-			    const char *channel, int id, const ImVec4 &col, double *x, double *y)
+			  [&](const char *channel, const ImVec4 &col, double *x, double *y)
 			{
 				ImPlot::Annotation(
 				  *x, *y, col, ImVec2(-10, -10), false, "%s", channel);
+				ImPlot::SetNextMarkerStyle(
+				  IMPLOT_AUTO, IMPLOT_AUTO, col, IMPLOT_AUTO, col);
+				ImPlot::PlotScatter(
+				  lak::fmt<"##{}scatter{}">(name, channel).c_str(), x, y, 1);
+			};
+
+			auto edit_point =
+			  [&](
+			    const char *channel, int id, const ImVec4 &col, double *x, double *y)
+			{
 				if constexpr (lak::is_const_v<PRIMARIES>)
 				{
 					LAK_UNUSED(id);
-					ImPlot::SetNextMarkerStyle(IMPLOT_AUTO, IMPLOT_AUTO, col);
-					ImPlot::PlotScatter(
-					  lak::fmt<"##{}scatter{}">(name, channel).c_str(), x, y, 1);
+					plot_point(channel, col, x, y);
 				}
 				else
 				{
+					ImPlot::Annotation(
+					  *x, *y, col, ImVec2(-10, -10), false, "%s", channel);
 					result |= ImPlot::DragPoint(id, x, y, col);
 				}
 			};
 
-			plot_point("R", 0, ImVec4(1, 0, 0, 1), x_coords + 0, y_coords + 0);
-			plot_point("G", 1, ImVec4(0, 1, 0, 1), x_coords + 1, y_coords + 1);
-			plot_point("B", 2, ImVec4(0, 0, 1, 1), x_coords + 2, y_coords + 2);
-			plot_point("W", 3, ImVec4(1, 1, 1, 1), x_coords + 3, y_coords + 3);
+			edit_point("R", 0, ImVec4(1, 0, 0, 1), x_coords + 0, y_coords + 0);
+			edit_point("G", 1, ImVec4(0, 1, 0, 1), x_coords + 1, y_coords + 1);
+			edit_point("B", 2, ImVec4(0, 0, 1, 1), x_coords + 2, y_coords + 2);
+			edit_point("W", 3, ImVec4(1, 1, 1, 1), x_coords + 3, y_coords + 3);
+
+			plot_point("C", ImVec4(0, 1, 1, 1), x_coords + 4, y_coords + 4);
+			plot_point("M", ImVec4(1, 0, 1, 1), x_coords + 5, y_coords + 5);
+			plot_point("Y", ImVec4(1, 1, 0, 1), x_coords + 6, y_coords + 6);
 
 			if constexpr (!lak::is_const_v<PRIMARIES>)
 				if (result)
